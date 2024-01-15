816c2968f2857f5e9aec277e52a105048a34b4e1
gerardodipascale <gerardodipascale@studenti.unisa.it>
2024-01-15 17:44:46 +0100

    first commit

---
+++ app/Http/Controllers/PazienteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paziente;
+use Illuminate\Http\Request;
+
+class PazienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Paziente $paziente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Paziente $paziente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Paziente $paziente)
+    {
+        //
+    }
+}
---
+++ app/Models/Questionario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Questionario extends Model
+{
+    use HasFactory;
+
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'Questionario';
+}
