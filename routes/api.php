7bfbdedbd5111177b91aa7aed0f6a9b342b31ae8
gerardodipascale <gerardodipascale@studenti.unisa.it>
2024-01-22 10:35:25 +0100

    Added api route to new controller

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MedicoController;
+use App\Http\Controllers\TerapiaController;
 use App\Http\Controllers\PazienteController;
+use App\Http\Controllers\EsercizioController;
+use App\Http\Controllers\EpisodioController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('medico',MedicoController::class);
+Route::resource('terapia',TerapiaController::class);
 Route::resource('paziente',PazienteController::class);
+Route::resource('esercizio',EsercizioController::class);
+Route::resource('episodio',EpisodioController::class);
