001de4f8cc0795963d0d3d82a43d31218aeccee3
gerardodipascale <gerardodipascale@studenti.unisa.it>
2024-02-04 15:42:15 +0100

    login con token, varie necessarie per login

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MedicoController;
 use App\Http\Controllers\TerapiaController;
 use App\Http\Controllers\PazienteController;
 use App\Http\Controllers\EsercizioController;
 use App\Http\Controllers\EpisodioController;
+use App\Http\Controllers\SigninUtente;
+use App\Models\Paziente;
 
 
 /*
@@ -20,12 +23,45 @@ use App\Http\Controllers\EpisodioController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->any('/checkToken', function (Request $request) {
+    return "hai un token";
 });
 
-Route::resource('medico',MedicoController::class);
-Route::resource('terapia',TerapiaController::class);
-Route::resource('paziente',PazienteController::class);
-Route::resource('esercizio',EsercizioController::class);
-Route::resource('episodio',EpisodioController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('medico',MedicoController::class);
+    Route::resource('terapia',TerapiaController::class);
+    Route::resource('paziente',PazienteController::class);
+    Route::resource('esercizio',EsercizioController::class);
+    Route::resource('episodio',EpisodioController::class);
+});
+
+
+
+//Route::post('/login', [SigninUtente::class, 'login']);
+
+
+/**
+* Middleware api serve per evitare il controllo su cross-site scripting
+* Si configura in http/Kernel.php
+*/
+Route::middleware('api')->group(function () {
+    /**
+    * Routes that require session data
+    */
+    Route::post('/login', function (Request $request, Response $response) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+     
+        $paziente = Paziente::where('email', $request->email)->first();
+     
+        if (! $paziente || ! password_verify($request->password, $paziente->password)) {
+            return $response;
+        }
+
+        $token = $paziente->createToken('paziente_token');
+        return ['token' => $token->plainTextToken];
+    })->name('login');
+});
